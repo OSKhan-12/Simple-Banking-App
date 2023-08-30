98182ee211c284da3195879000283436285c704f
OSKhan-12 <143180981+OSKhan-12@users.noreply.github.com>
2023-08-30 21:28:30 +0500

    Update Simple Banking App.py

--- Simple Banking App.py
+++ Simple Banking App.py
@@ -1,3 +1,5 @@
+## Remember this is just a Simulation not a real banking app 
+# Generate Random Numbers for Account no. and Balance
 import random
 tasks = ["1","2","3","4"]
 AC_no1 = random.randint(10, 19)
@@ -5,7 +7,9 @@ AC_no2 = random.randint(1234 , 9999)
 AC_no3 = random.randint(1234 , 9999)
 AC_no4 = random.randint(1234 , 9999)
 total = random.randint(100000 , 99999999)
+# Greets the user
 print("Hello!, Welcome to Khan Bank Limited.")
+# Card Number
 card = input("> Please Enter your Card Number: ")
 task = ""
 while task != "4":
@@ -16,6 +20,7 @@ while task != "4":
         else:
             print("Your Card Number must be from 14 to 16 digits only.")
             card = input("> Please enter a Valid number : ")
+    # Menu
     print('''What would you like to do Please type the number for the task you wish to proceed with:
     > 1 - Withdraw
     > 2 - Show Balance
@@ -25,9 +30,11 @@ while task != "4":
     while task not in tasks:
         print("> Please Type the number next to the tasks")
         task =input("> ")
+    # Exit (Option)
     if task == "4":
         input("Thanks for banking with us!")
         exit()
+    # Withdraw (Option)
     elif task == "1":
         draw = input("> How much would you like to withdraw?: Rs.")
         while True:
@@ -44,9 +51,11 @@ while task != "4":
                     input("Thanks for banking with us!")
                     total = last
                     break
+    # Show Balance (Option)
     if task == "2":
         print( f"> Your balance is Rs.{total}")
         input("Thanks for banking with us!")
+    # Transfer (Option)
     if task == "3":
         while True:
             AC = input("Please Enter the Account no. you wish to transfer to: ")
